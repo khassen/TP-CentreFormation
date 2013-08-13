729426aa08186c57bdf43acc8aa34b89f0c7c30a
AMICO Fabien <fabienamico@gmail.com>
2013-08-13 14:21:18 +0200

    Ajout du pom

---
+++ src/main/java/fr/treeptik/centreformation/runtime/Main.java
@@ -0,0 +1,28 @@
+package fr.treeptik.centreformation.runtime;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("****** Application ready ******");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+
+		Scanner scanner = new Scanner(System.in);
+
+		String line = scanner.nextLine();
+		
+		System.out.println(line);
+	}
+
+}
